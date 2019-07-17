f2ba6d5c7c173f98af6981ddb8dba1c9f5c2ca8b
Choih0401 <danielch0401@naver.com>
2019-07-17 22:22:46 +0900

    make login and register query

--- Server/routers/index.js
+++ Server/routers/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/connector.js');
+const login = require("./login.js");
 
 router.get('/', function(req, res){
   if (req.session.user === undefined){
     res.sendFile(__dirname + '/html/index.html');
   }
   else {
-    res.redirect('/lobby');
+    res.redirect('/html/404.html');
   }
 });
 
--- Server/routers/login.js
+++ Server/routers/login.js
@@ -2,12 +2,39 @@ const express = require("express");
 const router = express.Router();
 const db = require('../db/connector.js');
 
-router.post('/register', fuction(req, res){
+router.post('/register', function(req, res){
   let name = req.body.name;
-  let passwd = req.body.passwd;
-  let id = req.body.id;
+  let passwd = crypto.createHash('sha512').update(crypto.createHash('sha512').update(req.body.RegisterPasswd).digest('base64')).digest('base64');
+  let id = req.body.RegisterId;
   let resident = req.body.resident;
 
   const insertQuery = "INSERT INTO user (name, passwd, id, resident) VALUES(?, ?, ?, ?)";
-  console.log("[item/register]");
+  console.log("[login/register]");
+  db.query(insertQuery, [name, passwd, id, resident], function(err, result){
+    if(err) throw err;
+    return res.status(200).json({message:"Register Success"});
+  });
+});
+
+router.post('/login', function(req,res){
+  let id = req.body.LoginId;
+  let passwd = req.body.LoginPasswd;
+
+  const selectQuery = "SELECT * FROM user where id = ? and passwd = ?";
+  console.log("[login/login]");
+  db.query(selectQuery, [id, passwd], function(err, result){
+    if(err) throw err;
+    if(result.length == 0){
+      return res.status(401).json({message:"Login Fail"});
+    }else if(result[0].passwd == passwd){
+      req.session.user = {
+        id : id,
+        name : result[0].name,
+        authorized : true
+      };
+      return res.status(200).json({message:"Login Success"});
+    }else{
+      return res.status(401).json({message:"Login Fail"});
+    }
+  });
 });
