d13ee7fb8290cb7aa79ad515c461e42aa83e7daf
Choih0401 <danielch0401@naver.com>
2019-07-18 20:32:09 +0900

    5 TA

---
+++ Server/routers/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/connector.js');
+const login = require("./login.js");
+
+router.get('/', function(req, res){
+  if (req.session.user === undefined){
+    res.sendFile(__dirname + '/html/index.html');
+  }
+  else {
+    res.redirect('/html/404.html');
+  }
+});
+
+router.post('/blood',function(req, res){
+  let id = req.body.id;
+
+  const selectQuery = "SELECT * FROM blood where user = ?";
+  console.log("[index/blood]");
+  db.query(selectQuery, [id], function(err, result){
+    if(err) throw err;
+    return res.statue(200).json({list:result});
+  });
+});
+
+module.exports = router;
