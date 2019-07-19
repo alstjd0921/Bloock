a8868b5cbb371a1956a08124d90ffc1de0966b89
Choih0401 <danielch0401@naver.com>
2019-07-20 01:24:19 +0900

    오류 수정

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const db = require('./db/connector.js');
 const expressSession = require('express-session');
 const index = require('./routers/index.js');
 const login = require('./routers/login.js');
+const certreg = require('./routers/certreg.js');
 const bodyParser = require("body-parser");
 
 // view engine setup
