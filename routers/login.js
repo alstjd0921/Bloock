27fb3067063375e95d5a619ac5caa28bca8b2e55
Choih0401 <danielch0401@naver.com>
2019-07-20 01:35:16 +0900

    ???????

--- routers/login.js
+++ routers/login.js
@@ -19,7 +19,7 @@ router.get('/index', function(req, res){
 });
 
 router.get('/certreg',function(req, res){
-  res.sendFile(__dirname + '../views/certreg.html');
+  res.sendFile(__dirname, '../views/certreg.html');
 });
 
 router.post('/certreg', function(req, res){
