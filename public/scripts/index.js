57280f652301827b5a448dc7b8c5c2da0b4c7ba0
naruteride <tadanide@gmail.com>
2019-07-20 03:17:02 +0900

    인덱스 틀 맞춤

---
+++ public/scripts/index.js
@@ -0,0 +1,7 @@
+VanillaTilt.init(document.querySelector("#header-paper"), {
+    max: 20,
+    speed: 400
+});
+
+//It also supports NodeList
+VanillaTilt.init(document.querySelectorAll("#header-paper"));
