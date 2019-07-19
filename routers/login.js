3a248843aebf82d7ca5eebb307b2b6471828b18f
a-8-a <cogksmf02@naver.com>
2019-07-20 04:07:07 +0900

    shit

--- routers/login.js
+++ routers/login.js
@@ -53,7 +53,7 @@ router.post('/register', function(req, res){
   let id = req.body.RegisterId;
   let resident = req.body.resident;
   let ethwallet = web3.eth.accounts.create().privateKey;
-  let amount = web3.utils.toWei(0.5,"ether");
+  let amount = web3.utils.toWei('0.5',"ether");
   web3.eth.sendTransaction({from:share_account.address,to:ethwallet,value:str(amount)});
 
 
